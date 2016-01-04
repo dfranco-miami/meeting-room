ebf0d5a6f3e29a1a59303b398220221dd1849e66
Damian Franco <dfranco_miami@msn.com>
2016-01-04 18:40:26 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    mongoose = require('mongoose'),
+    expressSession = require('express-session'),
+    mongooseSession = require('mongoose-session'),  // https://github.com/chncdcksn/mongoose-session
+    //MongoStore = require('connect-mongo')(expressSession),
+    accountRoutes = require('./routes/account'),
+    bookingRoutes = require('./routes/bookings'),
+    app = express(),
+    port = 30000;
+
+
+var dbName = 'bookitDB';
+var connectionString = 'mongodb://localhost:27017/' + dbName;
+
+mongoose.connect(connectionString);
+
+app.use(expressSession({
+        key: 'session',
+        secret: '128013A7-5B9F-4CC0-BD9E-4480B2D3EFE9',
+        store: new mongooseSession(mongoose),
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api',  [accountRoutes, bookingRoutes]);
+
+var server = app.listen(port, function () {
+    console.log('Express server listening on port ' + server.address().port);
+});
